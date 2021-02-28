7bac42faa1814878a1928e13ba39b283a3e0ea91
Harsh Singh Bais <harshsbais5@gmail.com>
2021-02-28 15:26:03 +0530

    Add uuid as primary key and non editable

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -6,7 +6,8 @@ import uuid
 
 
 class Todo(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    uuid = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, unique=True, editable=False)
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
