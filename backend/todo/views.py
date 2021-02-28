83aac295f966c46f2250ad81d12f3453cff57519
Harsh Singh Bais <harshsbais5@gmail.com>
2021-02-28 18:03:47 +0530

    ReadOnly for non authenticated

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from .serializers import TodoSerializer
 from .models import Todo
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 # Create your views here.
 
 
@@ -11,4 +11,6 @@ class TodoView(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
+    # If you want to allow GET for everyone and all ops for authenticated users
+    permission_classes = [IsAuthenticatedOrReadOnly]
