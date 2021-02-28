c77c15e53fe9dfc62034781ed0339bf4291e08a7
Harsh Singh Bais <harshsbais5@gmail.com>
2021-02-28 12:59:10 +0530

    initial commit

---
+++ backend/todo/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+import uuid
+
+
+# Create your models here.
+
+
+class Todo(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
